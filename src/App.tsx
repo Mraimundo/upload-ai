1a9dd2137c00d3479180028d626549737ed2d32a
Mraimundo <mdumundo18@gmail.com>
2023-09-13 02:58:22 -0300

    feat: configuração do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <div>
+        <div>
+          <h1>upload.ai</h1>
+
+          <div>
+            <span>Desenvolvido com 💙 no NLW da Rocketseat</span>
+            <Button>Github</Button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
